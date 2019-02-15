2227aae732331cb24e9dbf3e7a8264607b850e86
OH SOUNG KWON <crystals8960@gmail.com>
2019-02-16 05:06:56 +0900

    UnityStart!

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = this.transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //this.transform.position = this.transform.position + new Vector3(0, 0, 1) * Time.deltaTime;
+            this.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //this.transform.position = this.transform.position + new Vector3(0, 0, 1) * Time.deltaTime;
+            this.transform.Translate(new Vector3(0, 0, -1) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            //this.transform.position = this.transform.position + new Vector3(0, 0, 1) * Time.deltaTime;
+            this.transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //this.transform.position = this.transform.position + new Vector3(0, 0, 1) * Time.deltaTime;
+            this.transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            //this.transform.eulerAngles = rotation;
+
+            //this.transform.Rotate(new Vector3(90, 0, 0) * Time.deltaTime);
+
+            rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(rotation);
+
+            //Debug.Log(transform.eulerAngles);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            //this.transform.eulerAngles = rotation;
+
+            //this.transform.Rotate(new Vector3(-90, 0, 0) * Time.deltaTime);
+
+            rotation += new Vector3(-90, 0, 0) * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(rotation);
+
+            //Debug.Log(transform.eulerAngles);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            //this.transform.eulerAngles = rotation;
+
+            //this.transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime);
+
+            rotation += new Vector3(0, -90, 0) * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(rotation);
+
+            //Debug.Log(transform.eulerAngles);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //rotation += new Vector3(90, 0, 0) * Time.deltaTime;
+            //this.transform.eulerAngles = rotation;
+
+            //this.transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+
+            rotation += new Vector3(0, 90, 0) * Time.deltaTime;
+            this.transform.rotation = Quaternion.Euler(rotation);
+
+            //Debug.Log(transform.eulerAngles);
+        }
+    }
+}
